36f98e00ce344e64707f12be9c2e120c6facbeb1
Ebonwrath <114185068+Ebonwrath@users.noreply.github.com>
2023-04-06 17:33:14 -0500

    commit

--- snnWeightConstraintV2.py
+++ snnWeightConstraintV2.py
@@ -4,16 +4,19 @@ from snntorch import spikeplot as splt
 from snntorch import spikegen
 from snntorch import utils
 
-import tonic
+import sys
 
 import torch
 dtype = torch.float
 import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 import numpy as np
+import itertools
 
 #@title Plotting Settings
 def plot_cur_mem_spk(cur, mem, spk, thr_line=False, vline=False, title=False, ylim_max1=1.25, ylim_max2=1.25):
@@ -67,83 +70,265 @@ def plot_snn_spikes(spk_in, spk1_rec, spk2_rec, title):
 
   plt.show()
 
+# dataloader arguments
+batch_size = 128
+data_path='/data/mnist'
 
-num_steps = 200
-data = np.random.choice([0, 1], size=(28, 28), p=[.1, .9])
+dtype = torch.float
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+# Define a transform
+transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0,), (1,))])
+
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+
+# Create DataLoaders
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)
+
+num_steps = 25
+# grid_pair = np.random.choice([0, 1], size=(28, 28), p=[.1, .9])
+grid_pair = np.load("D:\outfile.npy")
 
 num_inputs = 28*28
 num_hidden = 1000
 num_outputs = 10
 beta = 0.95
+k = False
 
-fc1 = nn.Linear(num_inputs, num_hidden)
-lif1 = snn.Leaky(beta=beta)
-fc2 = nn.Linear(num_hidden, num_outputs)
-lif2 = snn.Leaky(beta=beta)
+# Define Network
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
 
-# Initialize hidden states
-mem1 = lif1.init_leaky()
-mem2 = lif2.init_leaky()
+        # Initialize layers
+        self.fc1 = nn.Linear(num_inputs, num_hidden)
+        self.lif1 = snn.Leaky(beta=beta)
+        self.fc2 = nn.Linear(num_hidden, num_outputs)
+        self.lif2 = snn.Leaky(beta=beta)
 
-# record outputs
-mem2_rec = []
-spk1_rec = []
-spk2_rec = []
+    def forward(self, x):
 
-spk_in = spikegen.rate_conv(torch.rand((200, 784))).unsqueeze(1)
+        # Initialize hidden states at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
 
-for step in range(num_steps):
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            hasPair = False
-            #upper pair
-            if i > 0 and hasPair == False:
-                if data[i][j] == 1 and data[i-1][j] == 1:
-                    hasPair = True
+        # Record the final layer
+        spk2_rec = []
+        mem2_rec = []
 
-            #lower pair
-            if i < len(data)-1 and hasPair == False:
-                if data[i][j] == 1 and data[i+1][j] == 1:
-                    hasPair = True
+        for step in range(num_steps):
+          cur1 = self.fc1(x)
+          spk1, mem1 = self.lif1(cur1, mem1)
+          cur2 = self.fc2(spk1)
+          spk2, mem2 = self.lif2(cur2, mem2)
+          spk2_rec.append(spk2)
+          mem2_rec.append(mem2)
+           
+        return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
 
-            #left pair
-            if j > 0 and hasPair == False:
-                if data[i][j] == 1 and data[i][j-1] == 1:
-                    hasPair = True
+# Load the network onto CUDA if available
+net = Net().to(device)
 
-            #right pair
-            if j < len(data[i])-1 and hasPair == False:
-                if data[i][j] == 1 and data[i][j+1] == 1:
-                    hasPair = True
+# pass data into the network, sum the spikes over time
+# and compare the neuron with the highest number of spikes
+# with the target
 
-            #Neuron Generation
-            #Adjacent Pair found so Weight is normal
-            if(hasPair == True):
-                cur1 = fc1(spk_in[step]) # post-synaptic current <-- spk_in x weight
-                spk1, mem1 = lif1(cur1, mem1) # mem[t+1] <--post-syn current + decayed membrane
-                cur2 = fc2(spk1)
-                spk2, mem2 = lif2(cur2, mem2)
+def print_batch_accuracy(data, targets, train=False):
+    output, _ = net(data.view(batch_size, -1))
+    _, idx = output.sum(dim=0).max(1)
+    acc = np.mean((targets == idx).detach().cpu().numpy())
 
-                mem2_rec.append(mem2)
-                spk1_rec.append(spk1)
-                spk2_rec.append(spk2)
+    if train:
+        print(f"Train set accuracy for a single minibatch: {acc*100:.2f}%")
+    else:
+        print(f"Test set accuracy for a single minibatch: {acc*100:.2f}%")
+
+def train_printer(
+    data, targets, epoch,
+    counter, iter_counter,
+        loss_hist, test_loss_hist, test_data, test_targets):
+    print(f"Epoch {epoch}, Iteration {iter_counter}")
+    print(f"Train Set Loss: {loss_hist[counter]:.2f}")
+    print(f"Test Set Loss: {test_loss_hist[counter]:.2f}")
+    print_batch_accuracy(data, targets, train=True)
+    print_batch_accuracy(test_data, test_targets, train=False)
+    print("\n")
+
+loss = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.Adam(net.parameters(), lr=5e-4, betas=(0.9, 0.999))
+
+num_epochs = 1
+loss_hist = []
+test_loss_hist = []
+counter = 0
+
+# Outer training loop
+for epoch in range(num_epochs):
+    iter_counter = 0
+    train_batch = iter(train_loader)
+
+    # Minibatch training loop
+    for data, targets in train_batch:
+        data = data.to(device)
+        targets = targets.to(device)
+
+        # forward pass
+        net.train()
+        spk_rec, mem_rec = net(data.view(batch_size, -1))
+
+        # initialize the loss & sum over time
+        loss_val = torch.zeros((1), dtype=dtype, device=device)
+        for step in range(num_steps):
+            loss_val += loss(mem_rec[step], targets)
+
+        # Gradient calculation + weight update
+        if():
+          optimizer.zero_grad()
+          loss_val.backward()
+          optimizer.step()
+
+        else:
+          optimizer.zero_grad()
+          loss_val.backward()
+          optimizer.step() * 0 
+
+        # Store loss history for future plotting
+        loss_hist.append(loss_val.item())
+
+        # Test set
+        with torch.no_grad():
+            net.eval()
+            test_data, test_targets = next(iter(test_loader))
+            test_data = test_data.to(device)
+            test_targets = test_targets.to(device)
+            
+            # Test set forward pass
+            test_spk, test_mem = net(test_data.view(batch_size, -1))
+
+            # Test set loss
+            test_loss = torch.zeros((1), dtype=dtype, device=device)
+            for step in range(num_steps):
+                test_loss += loss(test_mem[step], test_targets)
+            test_loss_hist.append(test_loss.item())
+
+            # Print train/test loss/accuracy
+            if counter % 50 == 0:
+                train_printer(
+                    data, targets, epoch,
+                    counter, iter_counter,
+                    loss_hist, test_loss_hist,
+                    test_data, test_targets)
+            counter += 1
+            iter_counter +=1
+
+# Plot Loss
+fig = plt.figure(facecolor="w", figsize=(10, 5))
+plt.plot(loss_hist)
+plt.plot(test_loss_hist)
+plt.title("Loss Curves")
+plt.legend(["Train Loss", "Test Loss"])
+plt.xlabel("Iteration")
+plt.ylabel("Loss")
+plt.show()
+
+total = 0
+correct = 0
+
+# drop_last switched to False to keep all samples
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=False)
+
+with torch.no_grad():
+  net.eval()
+  for data, targets in test_loader:
+    data = data.to(device)
+    targets = targets.to(device)
+    
+    # forward pass
+    test_spk, _ = net(data.view(data.size(0), -1))
+
+    # calculate total accuracy
+    _, predicted = test_spk.sum(dim=0).max(1)
+    total += targets.size(0)
+    correct += (predicted == targets).sum().item()
+
+print(f"Total correctly classified test set images: {correct}/{total}")
+print(f"Test Set Accuracy: {100 * correct / total:.2f}%")
+
+# fc1 = nn.Linear(num_inputs, num_hidden)
+# lif1 = snn.Leaky(beta=beta)
+# fc2 = nn.Linear(num_hidden, num_outputs)
+# lif2 = snn.Leaky(beta=beta)
+
+# # Initialize hidden states
+# mem1 = lif1.init_leaky()
+# mem2 = lif2.init_leaky()
+
+# # record outputs
+# mem2_rec = []
+# spk1_rec = []
+# spk2_rec = []
+
+# spk_in = spikegen.rate_conv(torch.rand((200, 784))).unsqueeze(1)
+
+# for step in range(num_steps):
+#     for i in range(len(data)):
+#         for j in range(len(data[i])):
+#             hasPair = False
+#             #upper pair
+#             if i > 0 and hasPair == False:
+#                 if data[i][j] == 1 and data[i-1][j] == 1:
+#                     hasPair = True
+
+#             #lower pair
+#             if i < len(data)-1 and hasPair == False:
+#                 if data[i][j] == 1 and data[i+1][j] == 1:
+#                     hasPair = True
+
+#             #left pair
+#             if j > 0 and hasPair == False:
+#                 if data[i][j] == 1 and data[i][j-1] == 1:
+#                     hasPair = True
+
+#             #right pair
+#             if j < len(data[i])-1 and hasPair == False:
+#                 if data[i][j] == 1 and data[i][j+1] == 1:
+#                     hasPair = True
+
+#             #Neuron Generation
+#             #Adjacent Pair found so Weight is normal
+#             if(hasPair == True):
+#                 cur1 = fc1(spk_in[step]) # post-synaptic current <-- spk_in x weight
+#                 spk1, mem1 = lif1(cur1, mem1) # mem[t+1] <--post-syn current + decayed membrane
+#                 cur2 = fc2(spk1)
+#                 spk2, mem2 = lif2(cur2, mem2)
+
+#                 mem2_rec.append(mem2)
+#                 spk1_rec.append(spk1)
+#                 spk2_rec.append(spk2)
 
 
-            #Adjacent Pair not Found so Weight is set to 0
-            else:
-                for step in range(num_steps):
-                    cur1 = fc1(spk_in[step] * 0) # post-synaptic current <-- spk_in x weight
-                    spk1, mem1 = lif1(cur1, mem1) # mem[t+1] <--post-syn current + decayed membrane
-                    cur2 = fc2(spk1)
-                    spk2, mem2 = lif2(cur2, mem2)
+#             #Adjacent Pair not Found so Weight is set to 0
+#             else:
+#                 for step in range(num_steps):
+#                     cur1 = fc1(spk_in[step] * 0) # post-synaptic current <-- spk_in x weight
+#                     spk1, mem1 = lif1(cur1, mem1) # mem[t+1] <--post-syn current + decayed membrane
+#                     cur2 = fc2(spk1)
+#                     spk2, mem2 = lif2(cur2, mem2)
 
-                    mem2_rec.append(mem2)
-                    spk1_rec.append(spk1)
-                    spk2_rec.append(spk2)
+#                     mem2_rec.append(mem2)
+#                     spk1_rec.append(spk1)
+#                     spk2_rec.append(spk2)
 
-# convert lists to tensors
-mem2_rec = torch.stack(mem2_rec)
-spk1_rec = torch.stack(spk1_rec)
-spk2_rec = torch.stack(spk2_rec)
+# # convert lists to tensors
+# mem2_rec = torch.stack(mem2_rec)
+# spk1_rec = torch.stack(spk1_rec)
+# spk2_rec = torch.stack(spk2_rec)
 
-plot_snn_spikes(spk_in, spk1_rec, spk2_rec, "Fully Connected Spiking Neural Network")            
+# plot_snn_spikes(spk_in, spk1_rec, spk2_rec, "Fully Connected Spiking Neural Network")            
